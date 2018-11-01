5b49270171ed1535c4ba7b94a0252debfac64b28
YuliaMu <32544243+YuliaMu@users.noreply.github.com>
2018-11-01 19:12:37 +0300

    задание №2

--- sandbox/src/main/java/ru/stqa/first/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/first/sandbox/myFirstProgram.java
@@ -1,7 +1,14 @@
 package ru.stqa.first.sandbox;
 
-public class MyFirstProgram {
-    public static void main (String [] args) {
-        System.out.println("Hello, World!");
+public class myFirstProgram {
+    public static void main(String[] args){
+        Point p1 = new Point(2, 3);
+        Point p2 = new Point(4, 5);
+
+        double b = p1.distance(p1, p2);
+        System.out.println("Расстояние между двумя точками равно " + b);
+
+         b = p1.distance(p2);
+        System.out.println("Расстояние между двумя точками равно " + b);
     }
 }
