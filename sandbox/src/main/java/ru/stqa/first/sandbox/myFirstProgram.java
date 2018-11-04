da5175cb7da1134c8a97c31475a62ff1e63603f5
YuliaMu <32544243+YuliaMu@users.noreply.github.com>
2018-11-04 13:44:37 +0300

    перенесен static в класс Main

--- sandbox/src/main/java/ru/stqa/first/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/first/sandbox/myFirstProgram.java
@@ -5,10 +5,13 @@ public class myFirstProgram {
         Point p1 = new Point(2, 3);
         Point p2 = new Point(4, 5);
 
-        double b = p1.distance(p1, p2);
+        double b = distance(p1, p2);
         System.out.println("Расстояние между двумя точками равно " + b);
 
          b = p1.distance(p2);
         System.out.println("Расстояние между двумя точками равно " + b);
     }
+        public static double distance(Point p1, Point p2) {
+           return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+      }
 }
